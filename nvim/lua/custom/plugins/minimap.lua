6811066017b908caa52492e2716a81a164c2189e
Chris McGraw <cmcgraw@sonifi.com>
2024-05-06 11:15:33 -0500

    Adds minimap to nvim

---
+++ nvim/lua/custom/plugins/minimap.lua
@@ -0,0 +1,9 @@
+return {
+    'gorbit99/codewindow.nvim',
+    config = function()
+        local codewindow = require('codewindow')
+        codewindow.setup({ window_border = 'single', auto_enable = true, screen_bounds = "background" })
+        codewindow.apply_default_keybinds()
+        vim.api.nvim_set_hl(0, 'CodewindowBorder', { fg = '#2A2E40' })
+    end,
+}
