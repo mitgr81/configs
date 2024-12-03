cfe377b30facd32748bf04e99be952a5976adc28
Chris McGraw <cmcgraw@sonifi.com>
2024-12-03 16:53:40 -0600

    Adds auto-session and snacks

--- nvim/lua/custom/plugins/minimap.lua
+++ nvim/lua/custom/plugins/minimap.lua
@@ -2,7 +2,7 @@ return {
     'gorbit99/codewindow.nvim',
     config = function()
         local codewindow = require('codewindow')
-        codewindow.setup({ window_border = 'single', auto_enable = true, screen_bounds = "background" })
+        codewindow.setup({ window_border = 'single', auto_enable = false, screen_bounds = "background" })
         codewindow.apply_default_keybinds()
         vim.api.nvim_set_hl(0, 'CodewindowBorder', { fg = '#2A2E40' })
     end,
